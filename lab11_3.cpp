1db563284cb2a0a2909b18c671993ab7e15dfb55
Your Name <you@example.com>
2023-01-03 15:45:18 +0700

    finished lab11.3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    int n;
+    double sum,SS;
+    string textline;
+    while (getline(source,textline)){
+        n++;
+        sum+=atof(textline.c_str());
+        SS+=atof(textline.c_str())*atof(textline.c_str());
+    }
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/n << endl;
+    cout << "Standard deviation = " << sqrt(1.0/n*SS-sum*sum/n/n);
+}
